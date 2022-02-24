60c310f60687c784a709fb2bbfcbc311c75522ac
johnsloan1984 <johnsloan1984@gmail.com>
2022-02-24 12:26:58 +0000

    just Irish

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({limit: '100mb'}));
 
 app.get('/', (req, res) => {
   res.json({
